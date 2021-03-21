401820eb31804c53d00305d00063f535975cfd70
Jeffrey Su <sujeff@wustl.edu>
2021-03-21 16:07:22 -0500

    Wrote the prologue, added a function to check the response and a function to delay the print statements, and began Chapter 1

--- big-text-adventure/src/Game.java
+++ big-text-adventure/src/Game.java
@@ -12,12 +12,24 @@ public class Game {
 		}
 	}
 	
+	public static boolean checkResponse(String response) {
+		// This checks the response the user gives, and ensures it is within the allowed responses.
+		String[] allowedResponses = {"A", "B", "C"};
+		for (String answer : allowedResponses) {
+			if (response.equalsIgnoreCase(answer)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
 	public static void main(String[] args) {
 		
 		// TODO Start the game, ask the player for their name, and create the player
 		// TODO Maybe we should move this to a method or something so this doesn't become unruly.
-		System.out.println("====================");
+		
+		// PROLOGUE
+		System.out.println("============================================================");
 		System.out.println("You feel a warm breeze across your face as you enter the bazaar.");
 		delay(3);
 		System.out.println("Wait no, I think that was a fart from that guy. You cover your nose.");
@@ -27,6 +39,38 @@ public class Game {
 		String yourName = userInput.nextLine();
 		System.out.format("So what %s, you think you're funny or somethin?", yourName);
 		delay(2);
+		
+		// Canned response block, maybe we can simplify this somehow?
+		System.out.println("Choose a response: ");
+		System.out.println("A: Your fart smells like $%#*!");
+		System.out.println("B: Nope. *You continue walking along*");
+		System.out.println("C: Uh. Um. Uh. Yeah? Wait no. No I don't!");
+		String reply = userInput.nextLine();
+		while (checkResponse(reply) == false) {
+			System.out.println("Please enter a valid response by typing A, B, or C.");
+			reply = userInput.nextLine();
+		}
+		
+		delay(2);
+		System.out.println("A deep scowl appears on the man's face, almost as if nothing you said mattered.");
+		System.out.println("You see his fist come towards you and you wince");
+		System.out.println("but before you can make sense of the pain, everything turns black.");
+		delay(3);
+		System.out.println("Welcome to Big Text Adventure, created by Jeff Su, Ryan Miller, and Jonathan Feehan");
+		System.out.println("This is a text adventure game where you interact, fight, chat, and more all through text. Enjoy!");
+		delay(2);
+
+		// CHAPTER 1
+		System.out.println("..");
+		delay(1);
+		System.out.println(".....");
+		delay(2);
+		System.out.println(".........");
+		delay(3);
+		
+		
+
+
 		
 
 	}
