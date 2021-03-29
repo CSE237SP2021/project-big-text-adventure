d9620900189147dad190647eb8df898c6058d271
Feehanjt <feehanjt@wustl.edu>
2021-03-28 19:19:58 -0500

    Finished interface and added unit test to show interface works

---
+++ big-text-adventure/src/WeaponInterfaceTest.java
@@ -0,0 +1,39 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class WeaponInterfaceTest {
+
+	
+	/*
+	 * Shows that weapon getters work
+	 */
+	@Test
+	void TestDamageGetters() {
+		Weapon[] weaponList = new Weapon[3]; 
+		weaponList[0] = new BasicWeapon("Sword", 20, 5, 150, 2);
+		weaponList[1] = new BasicWeapon("Dagger" , 40 , 1, 150, 3);
+		weaponList[2] = new BasicWeapon("Staff of Flames", 30, 0, 300, 1);
+		assertAll ("Weapon Getters",
+				() -> assertEquals(20, weaponList[0].getDamage()),
+				() -> assertEquals(40, weaponList[1].getDamage()),
+				() -> assertEquals(30,weaponList[2].getDamage()));
+	}
+	
+	
+	
+	/*
+	 * Tests to make sure Interface works
+	 */
+	@Test
+	void TestInterface() {
+		Weapon weaponInterfaceTest = new BasicWeapon("Sword", 20, 5, 150, 2);
+		assertEquals(weaponInterfaceTest.getDamage(), getWeaponDamage(weaponInterfaceTest));
+	}
+	
+	
+	int getWeaponDamage(Weapon w) {
+		return w.getDamage();
+	}
+
+}
